9fa16260ec7d1e52cffc6ca117a8d6bc3fbfe49d
Christian Beneke <c.beneke@syseleven.de>
2019-07-18 14:21:43 +0200

    K8s-2593: Drop scrape of kube-state-metrics init container

--- api/pkg/resources/prometheus/configmap.go
+++ api/pkg/resources/prometheus/configmap.go
@@ -176,6 +176,9 @@ scrape_configs:
     # does not contain a common name for the pods ip address
     insecure_skip_verify: true
   relabel_configs:
+  - source_labels: [__meta_kubernetes_pod_label_app, __meta_kubernetes_pod_container_init]
+    regex: "kube-state-metrics;true"
+    action: drop
   - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_scrape_with_kube_cert]
     action: keep
     regex: true
