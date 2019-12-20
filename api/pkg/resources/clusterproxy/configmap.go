5f77a838f8822363c2cedf82eedd17f62a85c9d0
Bastian Hofmann <bashofmann@gmail.com>
2019-12-20 13:13:59 +0100

    Fix linkerd proxying of websocket connections (tap and top functionality in dashboard)
    The websocket library that linkerd uses compares the host header with the origin header if an origin header is present. Because of proxying they are not the same and we can just remove the origin header.

--- api/pkg/resources/clusterproxy/configmap.go
+++ api/pkg/resources/clusterproxy/configmap.go
@@ -85,6 +85,7 @@ const nginxConfig = `map $http_upgrade $connection_upgrade {
 		  proxy_pass http://$upstream$uri$is_args$args;
 		  proxy_http_version 1.1;
 		  proxy_pass_request_headers on;
+          proxy_set_header Origin "";
 		  proxy_set_header Upgrade $http_upgrade;
 		  proxy_set_header Connection $connection_upgrade;
 	  }
