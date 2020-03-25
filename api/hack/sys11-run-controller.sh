c164887306749481c627050c32188498f0a898c5
Olaf Klischat <o.klischat@syseleven.de>
2020-03-25 15:52:47 +0100

    sys11-run-controller fixed for 2.13

--- api/hack/sys11-run-controller.sh
+++ api/hack/sys11-run-controller.sh
@@ -95,34 +95,6 @@ while true; do
           #-oidc-issuer-client-secret=$(vault kv get -field=oidc-issuer-client-secret dev/seed-clusters/dev.kubermatic.io) \
           #-docker-pull-config-json-file=<generate the file>
 
-
-            -dynamic-datacenters=true \
-            -namespace=kubermatic \
-            -datacenter-name=europe-west3-c \
-            -kubeconfig=../../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
-            -versions=../config/kubermatic/static/master/versions.yaml \
-            -updates=../config/kubermatic/static/master/updates.yaml \
-            -kubernetes-addons-path=../addons \
-            -openshift-addons-path=../openshift_addons \
-            -openshift-addons-file=../config/kubermatic/static/master/openshift-addons.yaml \
-            -feature-gates=OpenIDAuthPlugin=true \
-            -worker-name="$(tr -cd '[:alnum:]' <<< $KUBERMATIC_WORKERNAME | tr '[:upper:]' '[:lower:]')" \
-            -external-url=dev.kubermatic.io \
-            -backup-container=../config/kubermatic/static/backup-container.yaml \
-            -cleanup-container=../config/kubermatic/static/cleanup-container.yaml \
-            -docker-pull-config-json-file=../../secrets/seed-clusters/dev.kubermatic.io/.dockerconfigjson \
-            -oidc-ca-file=../../secrets/seed-clusters/dev.kubermatic.io/caBundle.pem \
-            -oidc-issuer-url=$(vault kv get -field=oidc-issuer-url dev/seed-clusters/dev.kubermatic.io) \
-            -oidc-issuer-client-id=$(vault kv get -field=oidc-issuer-client-id dev/seed-clusters/dev.kubermatic.io) \
-            -oidc-issuer-client-secret=$(vault kv get -field=oidc-issuer-client-secret dev/seed-clusters/dev.kubermatic.io) \
-            -monitoring-scrape-annotation-prefix='kubermatic.io' \
-            -log-debug=$KUBERMATIC_DEBUG \
-            -log-format=Console \
-            -max-parallel-reconcile=10 \
-            -pprof-listen-address=":${PPROF_PORT}" \
-            -logtostderr \
-            -v=4 # Log-level for the Kube dependencies. Increase up to 9 to get request-level logs.
-
         PID=$!
     fi
 
